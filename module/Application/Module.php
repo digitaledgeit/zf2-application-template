ec0e14f244b25a3199b3035433e0f692b4e04dc6
James Newell <james@digitaledgeit.com.au>
2013-12-07 15:57:14 +1100

    applied formatting

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -1,39 +1,42 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
 
 namespace Application;
-
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
 
-class Module
-{
-    public function onBootstrap(MvcEvent $e)
-    {
-        $eventManager        = $e->getApplication()->getEventManager();
-        $moduleRouteListener = new ModuleRouteListener();
-        $moduleRouteListener->attach($eventManager);
-    }
+/**
+ * Module
+ * @author James Newell <james@digitaledgeit.com.au>
+ */
+class Module {
 
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function getConfig() {
+		return include __DIR__.'/config/module.config.php';
+	}
 
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function getAutoloaderConfig() {
+		return array(
+			'Zend\Loader\StandardAutoloader' => array(
+				'namespaces' => array(
+					__NAMESPACE__ => __DIR__.'/src/'.__NAMESPACE__,
+				),
+			),
+		);
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function onBootstrap(MvcEvent $event) {
+		$eventManager        = $event->getApplication()->getEventManager();
+		$moduleRouteListener = new ModuleRouteListener();
+		$moduleRouteListener->attach($eventManager);
+	}
+
 }
--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -3,8 +3,18 @@
 namespace Application\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 
+/**
+ * Module
+ * @author James Newell <james@digitaledgeit.com.au>
+ */
 class IndexController extends AbstractActionController {
+
+	/**
+	 * Index method
+	 * @return  mixed[string]
+	 */
 	public function indexAction() {
 		return array();
 	}
+
 }
